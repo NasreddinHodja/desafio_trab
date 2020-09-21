c4d23eb107e3ecec3b38517a776b67211415489b
NasreddinHodja <tbizetde@gmail.com>
2020-09-21 20:22:28 -0300

    value sold by month

--- etl.py
+++ etl.py
@@ -15,7 +15,7 @@ def format_date(date: str):
 
 def value_sold_by_contact():
     """ Generates DataFrame containing data for barchart of
-    total price sold by contact.
+    total price sold / contacts.
 
     Returns:
         pd.DataFrame containing ['contact_id', 'total_price']
@@ -32,9 +32,29 @@ def value_sold_by_contact():
     # get contact_name
     contacts = pd.read_csv('in/contacts.tsv', sep='\t', usecols=[0, 1])
     contacts.columns = ['contact_id', 'contact_name']
-
     chart_base = chart_base.merge(contacts, how='left')
 
-    return chart_base.filter(items=['contact_name', 'total_price'])
+    return chart_base.filter(items=['contact_id', 'contact_name',
+                                    'total_price'])
 
-print(value_sold_by_contact().head())
+def value_sold_by_month():
+    """ Generates DataFrame containing data for barchart of
+    total price sold / months.
+
+    Returns:
+        pd.DataFrame containing ['month', 'total_price']
+    """
+
+    deals = pd.read_csv('in/deals.tsv', sep='\t', usecols=[1, 2])
+    deals.columns = ['date', 'total_price']
+
+    deals['date'] = deals['date'].apply(format_date)
+
+    chart_base = (deals.groupby(['date'])
+                  .agg({'total_price': sum})
+                  .reset_index())
+
+    return chart_base
+
+
+print(value_sold_by_month().head())
